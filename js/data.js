2f92f7e035279634387bce01ad6660e7a64a473c
Karen Ical <79215104+Keibwi@users.noreply.github.com>
2022-08-04 22:33:18 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,4 @@
+export let grupoTarjetas1 = ["🍒", "🍓", "🥭", "🍌", "🍋", "🥝", "🍏", "🍇", "🥥"]
+export let grupoTarjetas2 = ["🍒", "🍓", "🥭", "🍌", "🍋", "🥝", "🍏", "🍇", "🥥"]
+
+export let todasLasTarjetas = grupoTarjetas1.concat(grupoTarjetas2);
