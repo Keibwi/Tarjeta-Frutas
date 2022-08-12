501ce0ab399ca61e93e4b2b02be8ac7d56cf41bf
Karen Ical <79215104+Keibwi@users.noreply.github.com>
2022-08-11 23:10:11 -0600

    Delete js directory

--- js/data.js
+++
@@ -1,4 +0,0 @@
-export let grupoTarjetas1 = ["🍒", "🍓", "🥭", "🍌", "🍋", "🥝", "🍏", "🍇", "🥥"]
-export let grupoTarjetas2 = ["🍒", "🍓", "🥭", "🍌", "🍋", "🥝", "🍏", "🍇", "🥥"]
-
-export let todasLasTarjetas = grupoTarjetas1.concat(grupoTarjetas2);
